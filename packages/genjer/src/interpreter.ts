425ea160f962d629af45113971f1efa9074575f8
Syaiful Bahri <syai.bahri6@gmail.com>
2019-11-16 22:05:43 +0700

    remove deps to @jonggrang/task

--- packages/genjer/src/interpreter.ts
+++ packages/genjer/src/interpreter.ts
@@ -1,5 +1,4 @@
 import {Either, isLeft} from '@jonggrang/prelude';
-import {Task, runTask} from '@jonggrang/task'
 import {EventQueue, Loop, stepper, withCont} from './event-queue';
 
 export function mergeInterpreter<F, G, I>(lhs: EventQueue<F, I>, rhs: EventQueue<G, I>): EventQueue<Either<F, G>, I> {
@@ -26,16 +25,11 @@ export function interpretNever<A>(): EventQueue<never, A> {
   return stepper(() => { throw new Error('never interpreter received input') });
 }
 
-export function interpretThroughTask<F, I>(nat: (f: F) => Task<I>, onErr: (e: Error) => void): EventQueue<F, I> {
-  return withCont((queue, i) => {
-    runTask(nat(i), (e, j) => {
-      if (e) return onErr(e);
-      queue.push(j as I);
+export function liftCont<F, I>(k: (c: (j: I) => void, fi: F) => void): EventQueue<F, I> {
+  return withCont((queue, f) => {
+    k(j => {
+      queue.push(j);
       queue.run();
-    });
+    }, f);
   });
-}
-
-export function interpretBasicTask<I>(onErr: (e: Error) => void): EventQueue<Task<I>, I> {
-  return interpretThroughTask(x => x, onErr);
 }
